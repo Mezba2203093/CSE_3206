435156a41ca2ddbd7874d5aa8ccf78d915b53b8a
Mezba Uddin Rubab <2203093@student.ruet.ac.bd>
2026-07-29 12:18:44 +0600

    Update main.cpp
    factorial

--- main.cpp
+++ main.cpp
@@ -1,7 +1,17 @@
 #include<bits/stdc++.h>
 using namespace std;
+int factorial(int n) {
+    if (n <= 1) return 1;
+    return n * factorial(n - 1);
+}
 int main()
 {
  cout<<"Hello World!"<<endl;
+ int num = 5;
+    int result = factorial(num);
+    cout << "Hello World! Factorial of " << num << " is " << result << endl;
+    return 0;
  
 }
+
+
